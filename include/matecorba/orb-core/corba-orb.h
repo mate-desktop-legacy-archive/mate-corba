Distrotech/ORBit2

Support for custom GMainContext from Ke Wang

include/orbit/orb-core/corba-orb.h
@@ -32,6 +32,8 @@ gboolean  ORBit_proto_use (const char *name);
  */
 glong ORBit_get_giop_recv_limit (void);
 
+void ORBit_set_giop_main_context (GMainContext *context);
+
 #ifdef ORBIT2_INTERNAL_API
 
 void      ORBit_ORB_forw_bind (CORBA_ORB                   orb,

src/orb/orb-core/corba-orb.c
@@ -1442,6 +1442,12 @@ ORBit_get_giop_recv_limit (void)
 	return giop_recv_get_limit ();
 }
 
+void
+ORBit_set_giop_main_context (GMainContext *context)
+{
+	giop_set_main_context (context);
+}
+
 const ORBit_option orbit_supported_options[] = {
 	{ "ORBid",              ORBIT_OPTION_STRING,  NULL }, /* FIXME: unimplemented */
 	{ "ORBImplRepoIOR",     ORBIT_OPTION_STRING,  NULL }, /* FIXME: unimplemented */

mate-desktop-legacy-archive/mate-corba

include/matecorba/orb-core/corba-orb.h
@@ -34,6 +34,8 @@ gboolean  MateCORBA_proto_use (const char *name);
  */
 glong MateCORBA_get_giop_recv_limit (void);
 
+void MateCORBA_set_giop_main_context (GMainContext *context);
+
 #ifdef MATECORBA2_INTERNAL_API
 
 void      MateCORBA_ORB_forw_bind (CORBA_ORB                   orb,

src/orb/orb-core/corba-orb.c
@@ -1442,6 +1442,12 @@ MateCORBA_get_giop_recv_limit (void)
 	return giop_recv_get_limit ();
 }
 
+void
+MateCORBA_set_giop_main_context (GMainContext *context)
+{
+	giop_set_main_context (context);
+}
+
 const MateCORBA_option matecorba_supported_options[] = {
 	{ "ORBid",              MATECORBA_OPTION_STRING,  NULL }, /* FIXME: unimplemented */
 	{ "ORBImplRepoIOR",     MATECORBA_OPTION_STRING,  NULL }, /* FIXME: unimplemented */